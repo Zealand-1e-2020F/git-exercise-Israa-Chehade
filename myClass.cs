3830de7bad2c2d7765a81f7967ea5ae04dea9761
Israa-Chehade <israaissam@hotmail.com>
2020-04-01 13:09:05 +0200

    Revert "Slettet myClass"
    This reverts commit 842e3e1d2690dc0449093c4cd7ffbc1727465a53.

---
+++ myClass.cs
@@ -0,0 +1,26 @@
+namespace GitExercise
+{
+    class myClass
+    {
+        public string Navn { get; set; }
+        public string Adresse { get; set; }
+        public int Postnummer { get; set; }
+        public string By { get; set;}
+
+        public myClass(string navn, string adresse, int postnummer, string by)
+        {
+            Navn = navn;
+            Adresse = adresse;
+            Postnummer = postnummer;
+            By = by;
+        }
+        public override string ToString()
+        {
+            return "Navnet er " + Navn + " adressen er " + Adresse + " postnr er " + Postnummer + " byen er " + By;
+        }
+
+        
+
+
+    }
+}
