eeb089ce8589b0d1e796a7306e43391ab09bbdef
Israa-Chehade <israaissam@hotmail.com>
2020-04-01 13:08:27 +0200

    Revert "opdateret program"
    This reverts commit 7da421172c6add3bedb8ba5572f56a78fd2c2801.

--- Program.cs
+++ Program.cs
@@ -8,8 +8,8 @@ namespace GitExercise
         {
             Console.WriteLine("Hello World!");
 
-            myClass2 myclass1 = new myClass2("Israa", "Birthevej 34", 2740, "Skovlunde");
-            myClass2 my1 = new myClass2("Chehade", "Ingevej 3", 2730, "København");
+            myClass myclass1 = new myClass("Israa", "Birthevej 34", 2740, "Skovlunde");
+            myClass my1 = new myClass("Chehade", "Ingevej 3", 2730, "København");
             myclass1.ToString();
 
             System.Console.WriteLine(myclass1.ToString());
